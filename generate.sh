fcb8fd609166d8fadec22f3b8641d38b5feb1d31
liujuepku <92344221+liujuepku@users.noreply.github.com>
2022-05-13 20:18:05 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 fairseq-generate s2t_nlpcc_ubart \
+CUDA_VISIBLE_DEVICES=1 fairseq-generate dataset \
 	--task translation_bert_gec \
-	--path chkpt/checkpoint_best.pt \
+	--path checkpoints/checkpoint_best.pt \
 	--batch-size 32 --beam 5 --remove-bpe \
